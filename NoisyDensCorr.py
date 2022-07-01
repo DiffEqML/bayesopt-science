8f1eea618923dc61c58d3aa4305a316a7e948d48
Khaled Younes <k.younes@live.com>
2022-07-01 16:13:33 -0700

    Noisy version added; clean-up and new data

--- NoisyDensCorr.py
+++ NoisyDensCorr.py
@@ -38,34 +38,46 @@ mpl.rcParams.update(nice_fonts)
 # device = torch.device("cpu")
 dtype = torch.double
 
-def loadmatlab(filename):
-    mat = scipy.io.loadmat('NoiseFree/'+filename, appendmat=True)
+def loadmatlab(filename, noise_flag):
+    mat = scipy.io.loadmat('NoisyData/'+filename, appendmat=True)
     mat_contents = mat[filename]
     N = len(mat_contents)
     P = np.zeros(N)
     corr_length = np.zeros(N)
     dens_fluc = np.zeros(N)
 
+    if noise_flag == True:
+        std_corr_length = np.zeros(N)
+        std_dens_fluc = np.zeros(N)
+
     for i in range(N):
         P[-1-i] = mat_contents[i][0]
         corr_length[-1-i] = mat_contents[i][1]
         dens_fluc[-1-i] = mat_contents[i][2]
 
-    return P, corr_length, dens_fluc
+        if noise_flag == True:
+            std_corr_length[-1-i] = mat_contents[i][3]
+            std_dens_fluc[-1-i] = mat_contents[i][4]
+
+    if noise_flag == True:
+        return P, corr_length, dens_fluc, std_corr_length, std_dens_fluc
+
+    else:
+        return P, corr_length, dens_fluc
 
 
 # Loading the data
 temperature = 30
 print(temperature)
 if type(temperature) == int:
-    P, corr_length, dens_fluc = loadmatlab('co2_t'+str(temperature)+'C')
+    P, corr_length, dens_fluc, std_corr_length, std_dens_fluc = loadmatlab('co2_t'+str(temperature)+'C', True)
 else:
     if temperature == 30.5:
-        P, corr_length, dens_fluc = loadmatlab('B2')
+        P, corr_length, dens_fluc, std_corr_length, std_dens_fluc = loadmatlab('co2_t'+'30p5', True)
     if temperature == 31.5:
-        P, corr_length, dens_fluc = loadmatlab('B1')
+        P, corr_length, dens_fluc, std_corr_length, std_dens_fluc = loadmatlab('co2_t'+'31p5', True)
     if temperature == 32.5:
-        P, corr_length, dens_fluc = loadmatlab('B3')
+        P, corr_length, dens_fluc, std_corr_length, std_dens_fluc = loadmatlab('co2_t'+'32p5', True)
 
 
 # Creating a cubic interpolation based on the gathered data
@@ -74,14 +86,15 @@ def f(x):
 
     return f_interp(x)
 
-def get_init(num_samples):
+def noise(x):
+    noise_interp = interp1d(P, std_corr_length, kind='cubic')
 
-    return random.sample(range(len(P)), num_samples)
-
+    return noise_interp(x)
 
 # For plotting purposes only
 plot_x = np.linspace(P.min(), P.max(), 1001)
 plot_y = f(plot_x)
+noiseplot_y = noise(plot_x)
 actual_max = plot_x[np.where(plot_y == plot_y.max())[0]]
 
 # Normalized pressure values
@@ -93,10 +106,12 @@ bounds = torch.stack([torch.zeros(1, dtype=dtype), torch.ones(1, dtype=dtype)])
 # Pick which samples to start with
 train_X = [[norm_P[0]], [norm_P[-1]]]
 train_Y = [[corr_length[0]], [corr_length[-1]]]
-# train_X = [[norm_P[0]], [norm_P[len(P)//2]]]
-# train_Y = [[corr_length[0]], [corr_length[len(P)//2]]]
+noise_Y = [[std_corr_length[0]], [std_corr_length[-1]]]
+
 train_X = torch.tensor(train_X, dtype=dtype)
 train_Y = torch.tensor(train_Y, dtype=dtype)
+noise_Y = torch.tensor(noise_Y, dtype=dtype)
+print(noise_Y)
 
 # Interactive plot to visualize BO moves
 plt.ion()
@@ -115,30 +130,22 @@ ax2.set_xlim(70, 82)
 plt.tight_layout()
 plt.pause(0.4)
 
-
 i = 1
 err = 1
 tol = 1e-2
 n_iter = 20
 x_span = torch.linspace(0, 1, 1001)[:, None, None] # batch, 1 (q), 1 (feature dimension)
-noise_variance = 1e-8
+noise_std = 1e-4
 
 # Main BO Loop
 while i <= n_iter and abs(err) > tol:
     # Two different ways of normalizing; keep training (generated) data stored separately
     norm_Y = (train_Y - train_Y.mean())/train_Y.std() # Mean and Std
     # norm_Y = (train_Y - train_Y.min())/(train_Y.max() - train_Y.min()) # Min Max
-
-    # Unnormalized
-    # norm_Y = train_Y
     
     # Fitting a GP model
-    # likelihood = GaussianLikelihood(noise_constraint=Interval(1e-8, 1e-7))
-    # likelihood = GaussianLikelihood(noise_constraint=GreaterThan(0.7))
-    # gp = SingleTaskGP(train_X, norm_Y, covar_module=RQKernel(), likelihood=likelihood)
-    # gp = SingleTaskGP(train_X, norm_Y, likelihood=likelihood)
-    # gp = FixedNoiseGP(train_X, norm_Y, covar_module=RQKernel(), train_Yvar=torch.full_like(norm_Y, 1e-8))
-    gp = FixedNoiseGP(train_X, norm_Y, train_Yvar=torch.full_like(norm_Y, noise_variance))
+    # noise_Y = torch.full_like(norm_Y, noise_std)
+    gp = FixedNoiseGP(train_X, norm_Y, train_Yvar=noise_Y**2)
     mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
     fit_gpytorch_model(mll)
 
@@ -149,26 +156,22 @@ while i <= n_iter and abs(err) > tol:
     candidate, _ = optimize_acqf(EI, bounds=bounds, q=1, num_restarts=5, raw_samples=1000)
 
     acq_eval = EI(x_span)
+    posterior = gp.posterior(x_span)
+    # print(posterior)
 
     # Calculate by how much the BO guess has moved by
     unnorm_candidate = candidate*(P.max() - P.min()) + P.min()
-    # prev_candidate = train_X[-1]*(P.max() - P.min()) + P.min()
 
-    # err = (unnorm_candidate - prev_candidate)/prev_candidate
     err = candidate - train_X[-1]
 
     print(i, unnorm_candidate.cpu().numpy(), f(unnorm_candidate), abs(err).cpu().numpy())
 
-    # norm_f_candidate = (f(unnorm_candidate) - corr_length.min())/(corr_length.max() - corr_length.min())
-    # norm_f_candidate = (f(unnorm_candidate) - train_Y.mean().cpu().numpy())/train_Y.std().cpu().numpy()
     # Append new torch tensors
     train_X = torch.cat((train_X, candidate))
 
-    # Normalized
-    # train_Y = torch.cat((train_Y, torch.tensor(norm_f_candidate)))
-
     # Unnormalized
     train_Y = torch.cat((train_Y, torch.tensor(f(unnorm_candidate), dtype=dtype)))
+    noise_Y = torch.cat((noise_Y, torch.tensor(noise(unnorm_candidate), dtype=dtype)))
 
     if i == 1:
         mylabel = 'BO Step'
@@ -184,16 +187,12 @@ while i <= n_iter and abs(err) > tol:
     ax1.plot(unnorm_candidate, f(unnorm_candidate), 'bs', markersize=8, label=mylabel)
     ax1.legend(frameon=False)
     plt.draw()
-    # ax1.pause(0.05)
-
 
     ax2.plot(plot_x, acq_eval.detach().numpy(), 'g', alpha=0.5, label=mylabel2)
     ax2.set(xlabel='$P~$[bar]', ylabel='Exp. Improvement')
-    # ax2.plot(plot_x[np.where(acq_eval.detach().numpy() == acq_eval.detach().numpy().max())[0]], 1, 'rx', markersize=8, label=mylabel3)
     ax2.legend(frameon=False)
     plt.draw()
     plt.tight_layout()
-    # ax2.tight_layout()
     plt.pause(0.25)
 
     if abs(err) > tol:
